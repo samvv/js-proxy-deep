49eab9003fdda2f716bc6c4bf7317bbb84e696ea
Sam Vervaeck <sam@vervaeck.be>
2016-12-23 15:41:08 +0100

    Add all traps

--- index.js
+++ index.js
@@ -16,86 +16,119 @@ class Node {
     this.root = root || target
 
     function pathPop(path) {
-      const chunks = _.toPath(path)
+      const parentPath = _.clone(path)
       const key = chunks[chunks.length-1]
-      chunks.pop()
-      return [chunks.join('.'), key]
+      parentPath.pop()
+      return [parentPath, key]
     }
 
     _.defaults(traps, {
-      get(oTarget, sPath, nest) {
-        const val = _.get(oTarget, sPath)
-        return (typeof val === 'object' || typeof val === 'string')
-          ? nest()
+      get(oRoot, sPath, oReceiver, fNest) {
+        const val = _.get(oRoot, sPath)
+        console.log(typeof val)
+        return (typeof val === 'object' || typeof val === 'function')
+          ? fNest()
           : val
       },
-      set(oTarget, sPath, vValue) {
-        _.set(oTarget, sPath, vValue)
+      set(oRoot, sPath, vValue) {
+        _.set(oRoot, sPath, vValue)
         return true
       },
-      deleteProperty(oTarget, sPath) {
-        _.unset(oTarget, sPath)
+      has(oRoot, sPath) {
+        return _.has(oRoot, sPath)
       },
-      has(oTarget, sPath) {
-        return _.has(oTarget, sPath)
+      deleteProperty(oRoot, sPath) {
+        _.unset(oRoot, sPath)
       },
-      defineProperty(oTarget, sPath, oDesc) {
+      enumerate: (oTarget, sPath) => {
+        console.log(sPath)
+      },
+      getPrototypeOf(target, path) {
+        return Object.getPrototypeOf(_.get(target, path))
+      },
+      setPrototypeOf(target, path) {
+        return Object.setPrototypeOf(_.get(target, path))
+      },
+      isExtensible(target, path) {
+        return Object.isExtensible(_.get(target, path))
+      },
+      preventExtensions(target, path) {
+        return Object.preventExtensions(_.get(target, path))
+      },
+      defineProperty(oRoot, sPath, oTarget, oDesc) {
         const [sParentPath, sKey] = pathPop(sPath)
-        return Object.defineProperty(get(oTarget, sParentPath), sKey, oDesc)
+        return Object.defineProperty(get(oRoot, sParentPath), sKey, oDesc)
       },
-      getOwnPropertyDescriptor(oTarget, sPath) {
+      getOwnPropertyDescriptor(oRoot, sPath) {
         const [sParentPath, sKey] = pathPop(sPath)
-        return Object.getOwnPropertyDescriptor(get(oTarget, sParentPath), sKey)
+        return Object.getOwnPropertyDescriptor(get(oRoot, sParentPath), sKey)
       },
-      apply(oTarget, sPath, thisArg, argumentsList) {
-        const fn = _.get(oTarget, sPath)
+      apply(oRoot, sPath, thisArg, argumentsList) {
+        console.log('tere')
+        const fn = _.get(oRoot, sPath)
         return Function.prototype.apply(thisArg, argumentsList)
+      },
+      construct(target, path, argumentsList, newTarget) {
+        return Reflect.construct(_.get(target, path), argumentsList, newTarget)
       }
     })
 
 		this.proxy = new Proxy(target, {
-      getPrototypeOf: traps.getPrototypeOf,
-      setPrototypeOf: traps.setPrototypeOf,
-      isExtensible: traps.isExtensible,
-      preventExtensions: traps.preventExtensions,
-      construct: traps.construct,
-      ownKeys: traps.ownKeys,
-			get: (oTarget, sKey) => {
+      getPrototypeOf: (target) => {
+        return traps.getPrototypeOf(this.root, this.path, target)
+      },
+      setPrototypeOf: (target, prototype) => {
+        return traps.setPrototypeOf(this.root, this.path, target, prototype)
+      },
+      isExtensible: (target) => {
+        return traps.isExtensible(this.root, this.path, target)
+      },
+      preventExtensions: (target) => {
+        return traps.preventExtensions(this.root, this.path, target)
+      },
+      construct: (target, argumentsList, newTarget) => {
+        return traps.construct(this.root, this.path, target, argumentsList, newTarget)
+      },
+      ownKeys: (target) => {
+        return traps.ownKeys(this.root, this.path, target)       
+      },
+			get: (oTarget, sKey, receiver) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-        return traps.get(this.root, newPath.join('.'), () => new Node({}, traps, newPath, this.root).proxy)
+        return traps.get(this.root, newPath, receiver, () => new Node({}, traps, newPath, this.root).proxy)
 			},
-			set: (oTarget, sKey, vValue) => {
+			set: (oTarget, sKey, vValue, receiver) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-        return traps.set(this.root, newPath.join('.'), vValue)
+        return traps.set(this.root, newPath, vValue, receiver)
 			},
 			deleteProperty: (oTarget, sKey) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-        return traps.deleteProperty(this.root, newPath.join('.'))
+        return traps.deleteProperty(this.root, newPath)
 			},
-			enumerate: (oTarget, sKey) => {
+			enumerate: (oTarget, sKey, reciever) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-        return traps.enumerate(this.root, newPath.join('.'))
+        return traps.enumerate(this.root, newPath)
 			},
-			has: (oTarget, sKey) => {
+			has: (oTarget, sKey, reciever) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-        return traps.has(this.root, newPath.join('.'))
+        return traps.has(this.root, newPath)
 			},
 			defineProperty: (oTarget, sKey, oDesc) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-				return traps.defineProperty(this.root, newPath.join('.'), oDesc)
+				return traps.defineProperty(this.root, newPath, oDesc)
 			},
 			getOwnPropertyDescriptor: (oTarget, sKey) => {
         const newPath = _.clone(this.path)
         newPath.push(sKey)
-        return traps.getOwnPropertyDescriptor(this.root, newPath.join('.'))
+        return traps.getOwnPropertyDescriptor(this.root, newPath)
 			},
       apply: (target, thisArg, argumentsList) => {
+        console.log('hre')
         return traps.apply(this.root, this.path, thisArg, argumentsList)
       }
 		})
